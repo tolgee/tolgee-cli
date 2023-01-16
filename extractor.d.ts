3dd2b6845dda5d01ab453aa9e69d83081e0a131e
Cynthia <cynthia@cynthia.dev>
2023-01-16 21:08:27 +0100

    feat: export types in `tolgee-cli/extractor` for custom extractors

---
+++ extractor.d.ts
@@ -0,0 +1,21 @@
+export type Key = {
+    keyName: string;
+    defaultValue?: string;
+    namespace?: string;
+};
+export type ExtractedKey = Key & {
+    line: number;
+};
+export type Warning = {
+    warning: string;
+    line: number;
+};
+export type Extractor = (fileContents: string, fileName: string) => string[];
+export type ExtractionResult = {
+    keys: ExtractedKey[];
+    warnings: Warning[];
+};
+export type ExtractionResults = Map<string, {
+    keys: ExtractedKey[];
+    warnings: Warning[];
+}>;
