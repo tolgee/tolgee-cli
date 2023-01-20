465676ef1957702b2aee938d28c3726969a68f80
Cynthia <cynthia@cynthia.dev>
2023-01-20 15:32:26 +0100

    fix: override `ansi.enabled` and prevent further overrides

--- test/e2e/__internal__/preload.ts
+++ test/e2e/__internal__/preload.ts
@@ -4,8 +4,10 @@
 
 import { tmpdir } from 'os';
 import { join } from 'path';
+import ansi from 'ansi-colors';
 
 import * as constants from '../../../src/constants';
 
 // @ts-expect-error
 constants.CONFIG_PATH = join(tmpdir(), '.tolgee-e2e');
+Object.defineProperty(ansi, 'enabled', { get: () => false, set: () => undefined })
