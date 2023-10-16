36659ca7def802224d127e57131ae80e5cd7de92
shixin.guo <shixin.guo@zoom.us>
2023-10-16 10:40:03 -0700

    feat: optimize error code

--- src/commands/pull.ts
+++ src/commands/pull.ts
@@ -40,9 +40,9 @@ async function pullHandler(this: Command, path: string) {
     success('Done!');
   } catch (e) {
     if (e instanceof HttpError && e.response.status === 400) {
-      const res = await e.response.text();
+      const res: any = await e.response.json();
       error(
-        `Please check if your parameters, including namespaces, are configured correctly. Tolgee responded with: ${res}`
+        `Please check if your parameters, including namespaces, are configured correctly. Tolgee responded with: ${res.code}`
       );
       return;
     }
