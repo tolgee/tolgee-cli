61f62a3072f66508ab22954508945b3e6467e6db
Jan Cizmar <cizmar@chlupac.com>
2025-12-12 17:10:20 +0100

    chore: eslint

--- test/e2e/pullWatch.test.ts
+++ test/e2e/pullWatch.test.ts
@@ -1,11 +1,7 @@
 import { setupTemporaryFolder, TMP_FOLDER } from './utils/tmp.js';
-import { run, runWithKill } from './utils/run.js';
+import { runWithKill } from './utils/run.js';
 import './utils/toMatchContentsOf.js';
-import {
-  createPak,
-  createProjectWithClient,
-  deleteProject,
-} from './utils/api/common.js';
+import { createPak, createProjectWithClient, deleteProject, } from './utils/api/common.js';
 import { TolgeeClient } from '#cli/client/TolgeeClient.js';
 import { PROJECT_1 } from './utils/api/project1.js';
 import { PullWatchUtil } from './utils/pullWatchUtil.js';
